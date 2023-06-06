9387b96c40fd87bb7e68b2e928a2a9d2f3dbdc7c
NiKToc <niktoc20@gmail.com>
2023-06-06 19:01:24 +0500

    task 19

---
+++ task 19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число:");
+string s = Console.ReadLine();
+
+if(s.Length == 5)
+{
+    if(s[0] == s[4] || s[1] == s[3])
+    Console.WriteLine("Палиндром");
+    else Console.WriteLine("Не палиндром");
+}
+else Console.WriteLine("Неверное число");
